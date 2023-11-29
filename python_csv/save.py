d0277619511ae2008ce472696bcff4d122e6cae2
Lucas Rodriguez <lucas.rodriguez@outlook.com>
2023-11-29 12:29:51 -0300

    Updated save.py to use StringIO with pd.read_html for Pandas compatibility

--- python_csv/save.py
+++ python_csv/save.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import os
+from io import StringIO
 
 def save_data(table, file_path):
-    df = pd.read_html(str(table))[0]
+    # Create the directory if it does not exist
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    # Wrap HTML in StringIO object
+    html_string = StringIO(str(table))
+
+    # Convert table to DataFrame and save as CSV
+    df = pd.read_html(html_string)[0]
     df.to_csv(file_path, index=False)
